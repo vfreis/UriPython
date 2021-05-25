e7df487534cc2418b7ac88e6429459683f6799eb
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2021-05-25 17:14:19 -0300

    study

---
+++ uri1044.py
@@ -0,0 +1,11 @@
+# Multiple function
+
+def multiple():
+    n1 , n2 = map(int, input().split())
+    
+    if n1 % n2 == 0 or n2 % n1 == 0:
+        print('Sao Multiplos')
+    else:
+        print('Nao sao Multiplos')
+
+multiple()
---
+++ uri1052.py
@@ -0,0 +1,7 @@
+def monthInt(number):
+    month = ['','January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December']
+    print(month[number])
+
+number = int(input())
+monthInt(number)
