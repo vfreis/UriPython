16750ab8ca2c9395e1538085f3761f06a72c25c8
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2021-05-27 14:27:48 -0300

    study

--- uri1166.py
+++ uri1166.py
@@ -1,3 +1,24 @@
-x1 = int(input())
-x2 = int(input())
+def rentDebt():
 
+    # Input Debt
+    debt = int(input())
+
+    #Input Payment
+    payment = int(input())
+
+    # Counter
+    count = 1
+
+    # Debt Payment Plan
+    while debt > 0:
+        print(f'pagamento: {count}')
+        print(f'antes = {debt}')
+        debt -= payment
+        if debt < 0:
+            debt = 0
+        print(f'depois = {debt}')
+        print('-----')
+        count += 1
+            
+
+rentDebt()
